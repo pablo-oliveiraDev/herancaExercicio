36b88d4f2158faa0f961b80cd1fb78d65ae3347c
Pablo de Oliveira Ferreira <pabloliverfe@gmail.com>
2021-01-18 02:03:06 -0300

    terminado class OutSourcedEmployee

---
+++ execicioHeranca1/Entities/OutSoucedEmployee.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace execicioHeranca1.Entities
+{
+    class OutSoucedEmployee : Employee
+    {
+        public double AdicionalCharge { get; set; }
+
+        public OutSoucedEmployee()
+        {
+
+        }
+        public OutSoucedEmployee(string name, int hours, double valuePerHour, double adicinalCharge) : base( name,  hours,  valuePerHour)
+        {
+            AdicionalCharge = adicinalCharge;
+        }
+
+        public double  payment(double Salary)
+        {
+            return Salary += AdicionalCharge * (110/100);
+        }
+    }
+}
