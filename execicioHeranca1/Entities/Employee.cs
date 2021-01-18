e0a1c70f1a391bfcba4a4f7a35d671a68e483e68
Pablo de Oliveira Ferreira <pabloliverfe@gmail.com>
2021-01-18 01:31:27 -0300

    class Employee feita

--- execicioHeranca1/Entities/Employee.cs
+++ execicioHeranca1/Entities/Employee.cs
@@ -6,5 +6,25 @@ namespace execicioHeranca1.Entities
 {
     class Employee
     {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour  { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double payment()
+        {
+            return ValuePerHour * Hours;
+        }
     }
 }
